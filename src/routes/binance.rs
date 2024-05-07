26ea49dba9907e4822ceaa6c12654a01a0788a17
Akash Shaw <shawakash2003@gmail.com>
2024-05-07 08:42:03 +0530

    chore: adds configurations for binance router

--- src/routes/binance.rs
+++ src/routes/binance.rs
@@ -3,9 +3,23 @@ use serde_json::json;
 
 use crate::types::utils::ResponseStatus;
 
-pub async fn binance() -> impl Responder {
+async fn binance() -> impl Responder {
     HttpResponse::Ok().json(json!({
         "message": "Binance API",
         "status": ResponseStatus::BadRequest.as_str(),
     }))
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/binance")
+            .route("/account", web::get().to(binance))
+            .route("/orders", web::get().to(binance))
+            .route("/order", web::post().to(binance))
+            .route("/order", web::delete().to(binance))
+            .route("/order", web::put().to(binance))
+            .route("/order", web::patch().to(binance))
+            .route("/order", web::head().to(binance))
+            .route("/order", web::trace().to(binance)),
+    );
 }
